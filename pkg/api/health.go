0221d87b38ba637b1725ed6dacc709d6b0a50ce1
Mats Bergmann <bateau@sea-shell.org>
2023-10-28 18:13:03 +0000

    style: tabs to spaces

--- pkg/api/health.go
+++ pkg/api/health.go
@@ -8,18 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//	@Summary		Get application health
-//	@Description	Get health status of application
-//	@Tags			Health
-//	@Accept			json
-//	@Produce		json
-//	@Success		200		{object}	models.Health	"desc"
-//	@Router			/health [get]
+//	@Summary        Get application health
+//	@Description    Get health status of application
+//	@Tags           Health
+//	@Accept         json
+//	@Produce        json
+//	@Success        200     {object}    models.Health   "desc"
+//	@Router	        /health [get]
 func ReturnHealth(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, models.Health{
-		Status:        "ok",
-		Name:          "GoGear-api",
-		Updated:       time.Now().Format("02.01.2006 15:04:05"),
-		Documentation: "https://github.com/SeaShell/gogear-api",
-	})
+    c.IndentedJSON(http.StatusOK, models.Health{
+        Status:        "ok",
+        Name:          "GoGear-api",
+        Updated:       time.Now().Format("02.01.2006 15:04:05"),
+        Documentation: "https://github.com/SeaShell/gogear-api",
+    })
 }
